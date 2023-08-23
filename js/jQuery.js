76aaf0290acdb014ecec9f887d69d242f02133c0
Artin-Simonian <artinsimonian@yahoo.com>
2023-08-23 13:21:06 -0700

    Finishing up the UI

--- js/jQuery.js
+++ js/jQuery.js
@@ -7,6 +7,28 @@ $('body').css({
 $('#to-do-list').css({
     backgroundColor: '#00FFF9',
     width: '700px',
-    
+    height: '80vh',
+    textAlign: 'center',
+    margin: ' 2rem auto',
+    borderRadius: '20px',
+    border: 'solid 2px #000'
+})
 
+$('#to-do-list > h1').css({
+    fontSize: '5rem',
+    color: '#fff',
+    textShadow: '#000 2px 2px'
+})
+
+$('#to-do-list > input').css({
+    fontSize: '3rem',
+    border: 'solid 2px #000'
+})
+
+$('#to-do-list > button').css({
+    margin: '1rem 0',
+    padding: '0.5rem 2rem',
+    fontSize: '2rem',
+    backgroundColor: '#AFFFFD',
+    border: 'solid 2px #000'
 })
